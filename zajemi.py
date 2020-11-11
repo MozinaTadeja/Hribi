e629b7e0c90edaacf3e5c52db9cbfd2c87328b85
Tadeja Mozina <tadeja.mozina@gmail.com>
2020-11-11 16:32:36 +0100

    Zajeti podatki

--- zajemi.py
+++ zajemi.py
@@ -1,6 +1,6 @@
-import requests
 import re
 import orodja
+
 
 vzorec_bloka = re.compile(
     r'<tr bgcolor.*?'
@@ -8,10 +8,10 @@ vzorec_bloka = re.compile(
 )
 
 vzorec = re.compile(
-    r'<tr bg.*/\d+/(?P<id>.*)">&nbsp;(?P<gora>.*)</a></td><td>&nbsp.*?">(?P<višina>.*?) m</a>.*width="(?P<priljubljenost>.*?)%.*</tr>'
+    r'<tr bg.*/\d+/(?P<id>.*)">&nbsp;(?P<gora>.*)</a></td><td>&nbsp.*?">(?P<višina>.*?) m</a>.*" width="(?P<priljubljenost>.*?)%.*</tr>'
 )
 
-ime_gorovja = re.compile(r'<title>(.*)</title>')
+#ime_gorovja = re.compile(r'<title>(.*)</title>')
 
 slovar_povezav = {"Goriško, Notranjsko in Snežniško hribovje" : "https://www.hribi.net/gorovje/gorisko_notranjsko_in_sneznisko_hribovje/26", "Julijske Alpe" : "https://www.hribi.net/gorovje/julijske_alpe/1", "Kamniško Savinjske Alpe" : "https://www.hribi.net/gorovje/kamnisko_savinjske_alpe/3", "Karavanke" : "https://www.hribi.net/gorovje/karavanke/11", "Pohorje in ostala severovzhodna Slovenija" : "https://www.hribi.net/gorovje/pohorje_in_ostala_severovzhodna_slovenija/4", "Polhograjsko hribovje in Ljubljana" : "https://www.hribi.net/gorovje/polhograjsko_hribovje_in_ljubljana/5", "Škofjeloško, Cerkljansko hribovje in Jelovica" : "https://www.hribi.net/gorovje/skofjelosko_cerkljansko_hribovje_in_jelovica/21", "Zasavsko - Posavsko hribovje in Dolenjska" : "https://www.hribi.net/gorovje/zasavsko_-_posavsko_hribovje_in_dolenjska/25"}
 
@@ -27,7 +27,10 @@ def uredi(blok, url):
     return gora
 
 def najdi(url):
-    datoteka = f'gorovja/{url[10:]}.html'
+    for i in slovar_povezav.keys():
+        if slovar_povezav[i] == url:
+            gorovje = i
+    datoteka = f'gorovja/{gorovje}.html'
     orodja.shrani_spletno_stran(url, datoteka)
     vsebina = orodja.vsebina_datoteke(datoteka)
     for blok in vzorec_bloka.finditer(vsebina):
